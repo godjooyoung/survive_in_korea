edbac3d58a7007b6556f127a6a8abd8ab7bd2e68
godjooyoung <xolovesjoo36@naver.com>
2026-02-04 03:32:51 +0900

    feat코드 변수명 리패곹링

--- js/app/main.js
+++ js/app/main.js
@@ -1,5 +1,5 @@
 // 게임 상태 관리
-let gameState = {
+let gameStateObj = {
 	timeLimit: 0, // 초 단위
 	timeRemaining: 0,
 	timerInterval: null,
@@ -11,13 +11,21 @@ let gameState = {
 	readTextCnt: 0,          // 누적 읽은 활자수 (episode.totalCnt 누적)
 	choiceMomentCnt: 0,      // choice 에피소드 진입 횟수
 	choiceOptionCnt: 0,      // 누적 선택지 수(choices.length 누적)
+
+	/**종료, 클리어 노드 */
+	endNodeId : "ep_end",
+	clearNodeId : "ep_clear"
 };
 
 let STORY = null;
 
-const endEpId = "ep_end";
-const clearEpId = "ep_clear";
+let bgmPlayer = new Audio();
+bgmPlayer.loop = true;
 
+let sfxPlayer = new Audio();
+sfxPlayer.loop = false;
+
+// 돔 요소
 const gameTitle = document.querySelector('.game_title');
 const imageWrap = document.querySelector('.game_image_wrap');
 const storyWrap = document.querySelector('.game_story_wrap');
@@ -27,37 +35,19 @@ const startBtn = document.querySelector('#game_start_btn');
 const statusBar = document.querySelector('.status_bar');
 
 
-let bgmPlayer = new Audio();
-bgmPlayer.loop = true;
-
-let sfxPlayer = new Audio();
-sfxPlayer.loop = false;
-
-
-// 스토리 데이터 패치 후 자바스크립트 객체로 변환해서 반환
-// async function loadStory() {
-// 	const res = await fetch("/data/story.json", { cache: "no-store" });
-// 	if (!res.ok) throw new Error("story.json load failed");
-// 	STORY = await res.json();
-// }
-
 // 스토리 데이터 패치 후 자바스크립트 객체로 변환해서 반환
 async function loadStory() {
-	console.log('yaml 파일 로드 내부 파일로 변경')
+	console.log('이야기를 가져옵니다.')
   const res = await fetch("/data/story.yaml", { cache: "no-store" });
   if (!res.ok) throw new Error("story.yaml load failed");
 
   const yamlText = await res.text();
   STORY = jsyaml.load(yamlText);
-
-	console.log("STORY start:", STORY?.start);
-	console.log("ep1:", STORY?.nodes?.ep1);
 }
-
 
 // 상태바
 function updateStatusBar() {
-	const percent = (gameState.timeRemaining / gameState.timeLimit) * 100;
+	const percent = (gameStateObj.timeRemaining / gameStateObj.timeLimit) * 100;
 	statusBar.style.width = percent + '%';
 }
 
@@ -84,10 +74,10 @@ function playSFX(src) {
 }
 
 // 배경 이미지 처리 함수 
-function updateSceneImage(prompt) {
+function updateSceneImage(pMsgObj) {
 	// 연출 처리를 위한 이미지 패스 뿐 아니라, 연출 정보도 매개변수로 받음
-	let imgPath   = prompt.img;
-	let imgEffect = prompt.sceneEffect;
+	let imgPath   = pMsgObj.img_src;
+	let imgEffect = pMsgObj.scn_fx;
 
 	if (imgPath) {
 		imageWrap.style.backgroundImage = `url(${imgPath})`;
@@ -128,19 +118,19 @@ function typeWriterEffect(element, htmlText, speed = 30, callback) {
 function startLifeTimer() {
 	const min = 3 * 60;
 	const max = 5 * 60;
-	gameState.timeLimit = Math.floor(Math.random() * (max - min + 1)) + min;
-	gameState.timeRemaining = gameState.timeLimit;
+	gameStateObj.timeLimit = Math.floor(Math.random() * (max - min + 1)) + min;
+	gameStateObj.timeRemaining = gameStateObj.timeLimit;
 
 	updateStatusBar(); // 시작 시 100%
 
-	gameState.timerInterval = setInterval(() => {
-		gameState.timeRemaining--;
+	gameStateObj.timerInterval = setInterval(() => {
+		gameStateObj.timeRemaining--;
 
 		updateStatusBar();
 
-		if (gameState.timeRemaining <= 0) {
-			clearInterval(gameState.timerInterval);
-			goToEpisode(endEpId);
+		if (gameStateObj.timeRemaining <= 0) {
+			clearInterval(gameStateObj.timerInterval);
+			runNode(gameStateObj.endNodeId);
 		}
 	}, 1000);
 }
@@ -149,34 +139,9 @@ function startLifeTimer() {
 // 시간이 남아있더라도 이 함수가 실행되면 더이상 실시간으로 생존 시간을 계산하지 않고 계산 로직 자체를 멈춘다.
 // 인터벌 객체 종료
 function pauseLifeTimer() {
-	if (!gameState.timerInterval) return;
-	clearInterval(gameState.timerInterval);
-	gameState.timerInterval = null;
-}
-
-
-// 통계 데이터를 위한 전역 상태 업데이트 하는 함수
-function applyEpisodeStats(ep) {
-	let epType = ep.type;
-	let epTextTotalCnt = ep.totalCnt;
-
-	// 1) 읽은 글자 수 누적
-	// 있을때 처리, 없거나 null이거나 undefiend 일땐 0으로 간주하고 더하지 않음. 0도 풀리쉬 값이므로 여기 해당 안됨
-	if (epTextTotalCnt) {
-		if (typeof epTextTotalCnt == "string") {
-			epTextTotalCnt = Number(epTextTotalCnt);
-		}
-		gameState.readTextCnt += epTextTotalCnt
-	}
-
-	// 2) 마주했던 선택 순간 횟수 누적
-	if (epType == "choice") {
-		gameState.choiceMomentCnt += 1;
-
-		if (Array.isArray(ep.choices)) {
-			gameState.choiceOptionCnt += ep.choices.length;
-		}
-	}
+	if (!gameStateObj.timerInterval) return;
+	clearInterval(gameStateObj.timerInterval);
+	gameStateObj.timerInterval = null;
 }
 
 
@@ -185,224 +150,271 @@ function applyEpisodeStats(ep) {
  * 게임 흐름 관련 함수
  */
 
-// 텍스트 다음 라인 진행
-function nextLine(node, index) {
-	const nextIndex = index + 1;
 
-	if (nextIndex < node.prompts.length) {
-		renderPrompt(node, nextIndex);
+function nextLine(pNodeObj, pStartIdx) {
+	// 마지막 인덱스가 아니라면 메세지 랜더
+	// 마지막 인덱스라면 다음 노드 런
+	
+	const nodeObj		= pNodeObj;
+		
+	const startIdx	= pStartIdx ? pStartIdx : 0;
+	const nextIdx 	= startIdx + 1;
+
+	const msgList = nodeObj.msg_list;
+
+	if (nextIdx < msgList.length) {
+		renderMsg(nodeObj, nextIdx);
 	} else {
-		goToEpisode(node.next);
+		runNode(nodeObj.next_ids);
 	}
 }
 
-// 프롬프트 랜더링 함수 -> nextLine 호출 nextLine에서 다시 renderPrompt 호출하기도 함. 즉 이거 두개는 서로 연관 
-function renderPrompt(node, index) {
 
-	const prompt = node.prompts[index];
-	
-	if (!prompt) return;
+function renderMsg(pNodeObj, pMsgIdx) {
+	const nodeObj = pNodeObj;
+	const msgIdx  = pMsgIdx;
+	const msgList = pNodeObj.msg_list;
 
-	const delayTime = prompt.introDelay?prompt.introDelay:0;
-	const isClear = node.id == clearEpId ? true : false;
-	const isLastPrompt = index == node.prompts.length - 1;
+	const nodeId = nodeObj.node_id;
+	const clearNodeId = gameStateObj.clearNodeId;
+	const isClearNode = nodeId == clearNodeId ? true : false;
 
+	const msgObj = msgList[msgIdx];
+
+	const msgType = msgObj.msg_tp ? msgObj.msg_tp : '';
+	const msgText = msgObj.msg_tx ? msgObj.msg_tx : '';
+	const msgDelayMs = msgObj.dly_ms ? msgObj.dly_ms : 0;
+
+	const isLastMsg = msgIdx == msgList.length - 1 ? true : false;
+
+	if (!msgObj) {
+		return;
+	}
+
+	// 돔 초기화
 	storyArea.innerHTML = '';
 	buttonWrap.innerHTML = ''; 
 	
-	//연출 다 제거
+	// 돔 초기화 - 연출 제거
 	imageWrap.classList.remove("shake", "shake__heavy");
 
 	// 이미지 처리
-	updateSceneImage(prompt);
-	
-	// TODO 딜레이타임 잇을경우 이미지 연출 후 딜레이 된다음 텍스트 연출
+	updateSceneImage(msgObj);
+
 	setTimeout(() => {
 		// 효과음 재생
-		playSFX(prompt.sfx);
+		playSFX(msgObj.sfx_src);
 
 		const bubble = document.createElement('div');
-		bubble.className = `story_bubble ${prompt.type}`;
+		bubble.className = `story_bubble ${msgType}`;
 
 		// 버블 돔 요소 붙이기
 		storyArea.appendChild(bubble);
 		
 		// 텍스트 출력 동작 부분
-		if (prompt.type == "normal_msg") {
-			typeWriterEffect(bubble, prompt.text, 25, afterTextRender);
-		} else {
-			bubble.innerHTML = prompt.text;
-			afterTextRender();
+		switch (msgType) {
+			case "normal_msg":
+				typeWriterEffect(bubble, msgText, 25, ()=>{afterTextRender()});
+				break;
+			case "system_msg":
+			case "effect_msg":
+				bubble.innerHTML = msgText;
+				afterTextRender();
+				break;
+			default:
+				console.debug(`msg_type를 확인하세요. ${msgType}.`);
 		}
-
-	}, delayTime);
-
-
-	// 데이터 타입이 라인일때 마지막 프롬프트까지 도달하면 다음 버튼 생성 하기 위한 내부 함수
-	function showNextButton() {
-		const btn = document.createElement('div');
-		btn.className = 'g_button';
-		btn.innerText = '다음';
-		btn.onclick = () => nextLine(node, index);
-		buttonWrap.appendChild(btn);
-	}
-
-	// 데이터 타입이 선택지 일때 마지막 프롬프트까지 도달하면 선택지 랜더링 하기 위한 내부 함수
-	function showChoiceButton(choices) {
-		buttonWrap.innerHTML = '';
-
-		const wrap = document.createElement('div');
-		wrap.className = 'choice_wrap';
-
-		let locked = false; // 한 번 선택하면 잠금
+	}, msgDelayMs);
 
 
-		choices.forEach(choice => {
-			const btn = document.createElement('div');
-			btn.className = 'g_button';
-			btn.innerText = choice.text;
-			/* 선택지 버튼 이벤트 바인딩 */
-			btn.onclick = () => {
-				if (locked) return;
-				locked = true;
-
-				// 선택한 버튼 색 변경된 거 보여주고,
-				// 나머지 선택 안된건 기존 색 유지, 즉 클릭된 애만 특정 css 클랙스명 추가해서 서택된 걸 사용자로 하여금 알도록 처리
-				// 1초 내지는 0.5초뒤에 다음 애피소드 이동 함수 
-
-				btn.classList.add("is_selected");
-				setTimeout(() => {
-					goToEpisode(choice.next);
-				}, 400);
-
-			};
-			wrap.appendChild(btn);
-		});
-
-		buttonWrap.appendChild(wrap);
-	}
-
-	// 클리어나 종료시 보일 마지막 버튼
-	function showClearButtons() {
-		buttonWrap.innerHTML = "";
-
-		const wrap = document.createElement("div");
-		wrap.className = "choice_wrap"; // 기존 스타일 재사용
-
-		// 1) 처음으로
-		const restartBtn = document.createElement("div");
-		restartBtn.className = "g_button";
-		restartBtn.innerText = "처음으로";
-		restartBtn.onclick = () => {
-			// TODO 저장 기능 완성 후
-			// saveGame();  // 있으면 저장 먼저 하고
-			window.location.reload();
-		};
-
-		// // 2) TODO 저장
-		// const saveBtn = document.createElement("div");
-		// saveBtn.className = "g_button";
-		// saveBtn.innerText = "저장";
-		// saveBtn.onclick = () => {
-		// 	// 일단 훅만 걸어두기: 저장 로직은 나중에
-		// 	// saveGame();
-		// 	console.log("SAVE:", gameState);
-		// };
-
-		wrap.appendChild(restartBtn);
-		// wrap.appendChild(saveBtn);
-		buttonWrap.appendChild(wrap);
-	}
-
-	// 주어진 프롬프트가 다 랜더링 되고 난 다음 동작할 함수
-	// 다음 버튼을 보여주거나, 선택지 버튼을 보여준다.
-	// 다음버튼이나 선택지 버튼엔 다른 에피소드로 갈수있는 연결 키가 존재
 	function afterTextRender() {
 
-		if(!isClear){
-			if (node.type == 'lines') {
-				showNextButton();
+		// 데이터 타입이 선택지 일때 마지막 프롬프트까지 도달하면 선택지 랜더링 하기 위한 내부 함수
+		function _showChoiceButton(choices) {
+			buttonWrap.innerHTML = '';
+
+			const wrap = document.createElement('div');
+			wrap.className = 'choice_wrap';
+
+			let locked = false; // 한 번 선택하면 잠금
+
+
+			choices.forEach(choice => {
+				const btn = document.createElement('div');
+				btn.className = 'g_button';
+				btn.innerText = choice.msg_tx;
+				/* 선택지 버튼 이벤트 바인딩 */
+				btn.onclick = () => {
+					if (locked) return;
+					locked = true;
+
+					// 선택한 버튼 색 변경된 거 보여주고,
+					// 나머지 선택 안된건 기존 색 유지, 즉 클릭된 애만 특정 css 클랙스명 추가해서 서택된 걸 사용자로 하여금 알도록 처리
+					// 1초 내지는 0.5초뒤에 다음 애피소드 이동 함수 
+
+					btn.classList.add("is_selected");
+					setTimeout(() => {
+						runNode(choice.next_ids);
+					}, 400);
+
+				};
+				wrap.appendChild(btn);
+			});
+
+			buttonWrap.appendChild(wrap);
+		}
+
+		// 클리어나 종료시 보일 마지막 버튼
+		function _showClearButtons() {
+			buttonWrap.innerHTML = "";
+
+			const wrap = document.createElement("div");
+			wrap.className = "choice_wrap"; // 기존 스타일 재사용
+
+			// 1) 처음으로
+			const restartBtn = document.createElement("div");
+			restartBtn.className = "g_button";
+			restartBtn.innerText = "처음으로";
+			restartBtn.onclick = () => {
+				// TODO 저장 기능 완성 후
+				// saveGame();  // 있으면 저장 먼저 하고
+				window.location.reload();
+			};
+
+			// // 2) TODO 저장
+			// const saveBtn = document.createElement("div");
+			// saveBtn.className = "g_button";
+			// saveBtn.innerText = "저장";
+			// saveBtn.onclick = () => {
+			// 	// 일단 훅만 걸어두기: 저장 로직은 나중에
+			// 	// saveGame();
+			// 	console.log("SAVE:", gameState);
+			// };
+
+			wrap.appendChild(restartBtn);
+			// wrap.appendChild(saveBtn);
+			buttonWrap.appendChild(wrap);
+		}
+
+		// 데이터 타입이 라인일때 마지막 프롬프트까지 도달하면 다음 버튼 생성 하기 위한 내부 함수
+		function _showNextButton() {
+			const btn = document.createElement('div');
+			btn.className = 'g_button';
+			btn.innerText = '다음';
+			btn.onclick = () => nextLine(nodeObj, msgIdx);
+			buttonWrap.appendChild(btn);
+		
+		}
+
+		// 통계 데이터를 위한 전역 상태 업데이트 하는 함수
+		function _applyPlayStats(pNodeObj) {
+			const nodeObj = pNodeObj;
+			let nodeType = nodeObj.node_tp;
+			let nodeTextTotalCnt = nodeObj.txt_cnt;
+
+			// 1) 읽은 글자 수 누적
+			// 있을때 처리, 없거나 null이거나 undefiend 일땐 0으로 간주하고 더하지 않음. 0도 풀리쉬 값이므로 여기 해당 안됨
+			if (nodeTextTotalCnt) {
+				if (typeof nodeTextTotalCnt == "string") {
+					nodeTextTotalCnt = Number(nodeTextTotalCnt);
+				}
+				gameStateObj.readTextCnt += nodeTextTotalCnt
 			}
 
-			if (node.type == 'choice' && isLastPrompt) {
-				showChoiceButton(node.choices);
+			// 2) 마주했던 선택 순간 횟수 누적
+			if (nodeType == "choice") {
+				gameStateObj.choiceMomentCnt += 1;
+
+				if (Array.isArray(nodeObj.choices)) {
+					gameStateObj.choiceOptionCnt += nodeObj.choices.length;
+				}
+			}
+		}
+
+		if(!isClearNode){
+			if (nodeObj.node_tp == 'lines') {
+				_showNextButton();
+			}
+
+			if (nodeObj.node_tp == 'choice' && isLastMsg) {
+				_showChoiceButton(nodeObj.choices);
 			}
 
 			// 타입이 그냥 줄글이든, 선택이든 마지막 프롬프트에 도달한 시점에 
 			// 통계 데이터 업데이트
-			if (isLastPrompt) {
-				applyEpisodeStats(node);
+			if (isLastMsg) {
+				_applyPlayStats(nodeObj);
 			}
 		}else{
-			showClearButtons()
+			_showClearButtons()
 		}
 		
 	}
 
 }
 
-// 에피소드 랜더링 함수
-function renderEpisode(epId) {
-	let node = STORY.nodes[epId];
+// 스토리 노드 실행
+function runNode(pNodeIds) {
+	const nodeIds = pNodeIds;
+	let nodeId;
 
-	if (!node) {
-		// 노드 객체가 없으면 타이머 종료후 클리어로 이동
-		pauseLifeTimer()
-		node = STORY.nodes[clearEpId];
+	if (!nodeIds || (Array.isArray(nodeIds) && nodeIds.length == 0)) {
+		// 1) epIds가 없거나 빈 배열이면 → 클리어 루트
+		nodeId = null;
+	} else if (Array.isArray(nodeIds)) {
+		// 2) 배열이면 랜덤 선택
+		const randIndex = Math.floor(Math.random() * nodeIds.length);
+		nodeId = nodeIds[randIndex];
+	} else if (typeof pNodeIds == "string") {
+		// 3) 문자열이면 그대로
+		nodeId = nodeIds;
+	} else {
+		// 4) 그 외 타입은 방어
+		nodeId = null;
 	}
+
+	gameStateObj.currentEpisode = nodeId;
+	renderNode(nodeId);
+}
+
+// 스토리 노드 랜더링
+function renderNode(nodeId) {
+	let nodeObj = STORY.node_dict[nodeId];
+
+	if (!nodeObj) {
+		pauseLifeTimer();
+		nodeObj = STORY.node_dict[gameStateObj.clearNodeId];
+	}
+
+	let nodeBgm = nodeObj.bgm_src ? nodeObj.bgm_src : null;
 
 	storyArea.innerHTML = '';
 	buttonWrap.innerHTML = '';
 
 	// BGM 변경
-	if (node.bgm) {
-		playBGM(node.bgm);
+	if (nodeBgm) {
+		playBGM(nodeBgm);
 	}
 
-	// 첫 프롬프트 출력
-	renderPrompt(node, 0);
+	renderMsg(nodeObj, 0);
 }
 
-// 에피소드 이동 함수
-function goToEpisode(epIds) {
-	let epId;
 
-	// 기존 스트링 형태에서 문자열 배열 형태로 변경
-	if (!epIds || (Array.isArray(epIds) && epIds.length == 0)) {
-		// 1) epIds가 없거나 빈 배열이면 → 클리어 루트
-		epId = null;
-	} else if (Array.isArray(epIds)) {
-		// 2) 배열이면 랜덤 선택
-		const randIndex = Math.floor(Math.random() * epIds.length);
-		epId = epIds[randIndex];
-	} else if (typeof epIds == "string") {
-		// 3) 문자열이면 그대로
-		epId = epIds;
-	} else {
-		// 4) 그 외 타입은 방어
-		epId = null;
-	}
-
-	gameState.currentEpisode = epId;
-	gameState.currentPromptIndex = 0;
-	renderEpisode(epId);
-}
 
 
 // 게임 시작 버튼 클릭
 startBtn.addEventListener('click', async () => {
 	// TODO 로드 스토리 안될때 트라이 캐치로 예외걸기
-	playBGM("/assets/audio/bgm/theme_008.mp3");
+	playBGM("/assets/audios/bgm/theme_008.mp3");
 
 	// 노필터 삭제하기
 	imageWrap.classList.remove('no_filter');
 	gameTitle.style.display = 'none';
 	await loadStory();
 	startLifeTimer();     // 생존 타이머 시작
-	goToEpisode('ep1');   // 무조건 ep1 시작
+	runNode('ep1');   // 무조건 ep1 시작
 });
 
-// goToEpisode ->  renderEpisode -> renderPrompt (showNextButton, renderChoices / 통계 누적) 
 
 
 
---
+++ js/app/main_old.js
@@ -0,0 +1,415 @@
+// 게임 상태 관리
+let gameState = {
+	timeLimit: 0, // 초 단위
+	timeRemaining: 0,
+	timerInterval: null,
+	currentEpisode: "ep1",
+	currentChoosenCnt: 0,
+	currentReadTextCnt: 0,
+
+	/* 통계값 */
+	readTextCnt: 0,          // 누적 읽은 활자수 (episode.totalCnt 누적)
+	choiceMomentCnt: 0,      // choice 에피소드 진입 횟수
+	choiceOptionCnt: 0,      // 누적 선택지 수(choices.length 누적)
+};
+
+let STORY = null;
+
+const endEpId = "ep_end";
+const clearEpId = "ep_clear";
+
+const gameTitle = document.querySelector('.game_title');
+const imageWrap = document.querySelector('.game_image_wrap');
+const storyWrap = document.querySelector('.game_story_wrap');
+const storyArea = document.querySelector('.story_area');
+const buttonWrap = document.querySelector('.button_wrap');
+const startBtn = document.querySelector('#game_start_btn');
+const statusBar = document.querySelector('.status_bar');
+
+
+let bgmPlayer = new Audio();
+bgmPlayer.loop = true;
+
+let sfxPlayer = new Audio();
+sfxPlayer.loop = false;
+
+
+// 스토리 데이터 패치 후 자바스크립트 객체로 변환해서 반환
+// async function loadStory() {
+// 	const res = await fetch("/data/story.json", { cache: "no-store" });
+// 	if (!res.ok) throw new Error("story.json load failed");
+// 	STORY = await res.json();
+// }
+
+// 스토리 데이터 패치 후 자바스크립트 객체로 변환해서 반환
+async function loadStory() {
+	console.log('yaml 파일 로드 내부 파일로 변경')
+  const res = await fetch("/data/story.yaml", { cache: "no-store" });
+  if (!res.ok) throw new Error("story.yaml load failed");
+
+  const yamlText = await res.text();
+  STORY = jsyaml.load(yamlText);
+
+	console.log("STORY start:", STORY?.start);
+	console.log("ep1:", STORY?.nodes?.ep1);
+}
+
+
+// 상태바
+function updateStatusBar() {
+	const percent = (gameState.timeRemaining / gameState.timeLimit) * 100;
+	statusBar.style.width = percent + '%';
+}
+
+// 배경음 재생
+function playBGM(src) {
+	if (bgmPlayer.src.includes(src)) return;
+	bgmPlayer.src = src;
+	bgmPlayer.volume = 0.3;
+	bgmPlayer.play().catch(() => { });
+}
+
+// 효과음 재생
+function playSFX(src) {
+	if (!src) {
+		// 새 sfx 없으면 기존 소리 정지
+		sfxPlayer.pause();
+		sfxPlayer.currentTime = 0;
+		return;
+	}
+
+	sfxPlayer.src = src;
+	sfxPlayer.volume = 0.6;
+	sfxPlayer.play().catch(() => { });
+}
+
+// 배경 이미지 처리 함수 
+function updateSceneImage(prompt) {
+	// 연출 처리를 위한 이미지 패스 뿐 아니라, 연출 정보도 매개변수로 받음
+	let imgPath   = prompt.img;
+	let imgEffect = prompt.sceneEffect;
+
+	if (imgPath) {
+		imageWrap.style.backgroundImage = `url(${imgPath})`;
+		imageWrap.style.backgroundColor = 'transparent';
+		if(imgEffect){
+			imageWrap.classList.add(imgEffect);
+		}
+	} else {
+		imageWrap.style.backgroundImage = 'none';
+		imageWrap.style.backgroundColor = '#0A0D13'; // 기본 단색
+	}
+}
+
+// 타이핑 함수
+function typeWriterEffect(element, htmlText, speed = 30, callback) {
+	let i = 0;
+	let text = htmlText.replace(/<br\s*\/?>/gi, "\n"); // 줄바꿈 처리
+	element.innerHTML = "";
+
+	function type() {
+		if (i < text.length) {
+			if (text[i] == "\n") {
+				element.innerHTML += "<br/>";
+			} else {
+				element.innerHTML += text[i];
+			}
+			i++;
+			setTimeout(type, speed);
+		} else if (callback) {
+			callback();
+		}
+	}
+
+	type();
+}
+
+// 생존시간 함수
+function startLifeTimer() {
+	const min = 3 * 60;
+	const max = 5 * 60;
+	gameState.timeLimit = Math.floor(Math.random() * (max - min + 1)) + min;
+	gameState.timeRemaining = gameState.timeLimit;
+
+	updateStatusBar(); // 시작 시 100%
+
+	gameState.timerInterval = setInterval(() => {
+		gameState.timeRemaining--;
+
+		updateStatusBar();
+
+		if (gameState.timeRemaining <= 0) {
+			clearInterval(gameState.timerInterval);
+			goToEpisode(endEpId);
+		}
+	}, 1000);
+}
+
+
+// 시간이 남아있더라도 이 함수가 실행되면 더이상 실시간으로 생존 시간을 계산하지 않고 계산 로직 자체를 멈춘다.
+// 인터벌 객체 종료
+function pauseLifeTimer() {
+	if (!gameState.timerInterval) return;
+	clearInterval(gameState.timerInterval);
+	gameState.timerInterval = null;
+}
+
+
+// 통계 데이터를 위한 전역 상태 업데이트 하는 함수
+function applyEpisodeStats(ep) {
+	let epType = ep.type;
+	let epTextTotalCnt = ep.totalCnt;
+
+	// 1) 읽은 글자 수 누적
+	// 있을때 처리, 없거나 null이거나 undefiend 일땐 0으로 간주하고 더하지 않음. 0도 풀리쉬 값이므로 여기 해당 안됨
+	if (epTextTotalCnt) {
+		if (typeof epTextTotalCnt == "string") {
+			epTextTotalCnt = Number(epTextTotalCnt);
+		}
+		gameState.readTextCnt += epTextTotalCnt
+	}
+
+	// 2) 마주했던 선택 순간 횟수 누적
+	if (epType == "choice") {
+		gameState.choiceMomentCnt += 1;
+
+		if (Array.isArray(ep.choices)) {
+			gameState.choiceOptionCnt += ep.choices.length;
+		}
+	}
+}
+
+
+
+/**
+ * 게임 흐름 관련 함수
+ */
+
+// 텍스트 다음 라인 진행
+function nextLine(node, index) {
+	const nextIndex = index + 1;
+
+	if (nextIndex < node.prompts.length) {
+		renderPrompt(node, nextIndex);
+	} else {
+		goToEpisode(node.next);
+	}
+}
+
+// 프롬프트 랜더링 함수 -> nextLine 호출 nextLine에서 다시 renderPrompt 호출하기도 함. 즉 이거 두개는 서로 연관 
+function renderPrompt(node, index) {
+
+	const prompt = node.prompts[index];
+	
+	if (!prompt) return;
+
+	const delayTime = prompt.introDelay?prompt.introDelay:0;
+	const isClear = node.id == clearEpId ? true : false;
+	const isLastPrompt = index == node.prompts.length - 1;
+
+	storyArea.innerHTML = '';
+	buttonWrap.innerHTML = ''; 
+	
+	//연출 다 제거
+	imageWrap.classList.remove("shake", "shake__heavy");
+
+	// 이미지 처리
+	updateSceneImage(prompt);
+	
+	// TODO 딜레이타임 잇을경우 이미지 연출 후 딜레이 된다음 텍스트 연출
+	setTimeout(() => {
+		// 효과음 재생
+		playSFX(prompt.sfx);
+
+		const bubble = document.createElement('div');
+		bubble.className = `story_bubble ${prompt.type}`;
+
+		// 버블 돔 요소 붙이기
+		storyArea.appendChild(bubble);
+		
+		// 텍스트 출력 동작 부분
+		if (prompt.type == "normal_msg") {
+			typeWriterEffect(bubble, prompt.text, 25, afterTextRender);
+		} else {
+			bubble.innerHTML = prompt.text;
+			afterTextRender();
+		}
+
+	}, delayTime);
+
+
+	// 데이터 타입이 라인일때 마지막 프롬프트까지 도달하면 다음 버튼 생성 하기 위한 내부 함수
+	function showNextButton() {
+		const btn = document.createElement('div');
+		btn.className = 'g_button';
+		btn.innerText = '다음';
+		btn.onclick = () => nextLine(node, index);
+		buttonWrap.appendChild(btn);
+	}
+
+	// 데이터 타입이 선택지 일때 마지막 프롬프트까지 도달하면 선택지 랜더링 하기 위한 내부 함수
+	function showChoiceButton(choices) {
+		buttonWrap.innerHTML = '';
+
+		const wrap = document.createElement('div');
+		wrap.className = 'choice_wrap';
+
+		let locked = false; // 한 번 선택하면 잠금
+
+
+		choices.forEach(choice => {
+			const btn = document.createElement('div');
+			btn.className = 'g_button';
+			btn.innerText = choice.text;
+			/* 선택지 버튼 이벤트 바인딩 */
+			btn.onclick = () => {
+				if (locked) return;
+				locked = true;
+
+				// 선택한 버튼 색 변경된 거 보여주고,
+				// 나머지 선택 안된건 기존 색 유지, 즉 클릭된 애만 특정 css 클랙스명 추가해서 서택된 걸 사용자로 하여금 알도록 처리
+				// 1초 내지는 0.5초뒤에 다음 애피소드 이동 함수 
+
+				btn.classList.add("is_selected");
+				setTimeout(() => {
+					goToEpisode(choice.next);
+				}, 400);
+
+			};
+			wrap.appendChild(btn);
+		});
+
+		buttonWrap.appendChild(wrap);
+	}
+
+	// 클리어나 종료시 보일 마지막 버튼
+	function showClearButtons() {
+		buttonWrap.innerHTML = "";
+
+		const wrap = document.createElement("div");
+		wrap.className = "choice_wrap"; // 기존 스타일 재사용
+
+		// 1) 처음으로
+		const restartBtn = document.createElement("div");
+		restartBtn.className = "g_button";
+		restartBtn.innerText = "처음으로";
+		restartBtn.onclick = () => {
+			// TODO 저장 기능 완성 후
+			// saveGame();  // 있으면 저장 먼저 하고
+			window.location.reload();
+		};
+
+		// // 2) TODO 저장
+		// const saveBtn = document.createElement("div");
+		// saveBtn.className = "g_button";
+		// saveBtn.innerText = "저장";
+		// saveBtn.onclick = () => {
+		// 	// 일단 훅만 걸어두기: 저장 로직은 나중에
+		// 	// saveGame();
+		// 	console.log("SAVE:", gameState);
+		// };
+
+		wrap.appendChild(restartBtn);
+		// wrap.appendChild(saveBtn);
+		buttonWrap.appendChild(wrap);
+	}
+
+	// 주어진 프롬프트가 다 랜더링 되고 난 다음 동작할 함수
+	// 다음 버튼을 보여주거나, 선택지 버튼을 보여준다.
+	// 다음버튼이나 선택지 버튼엔 다른 에피소드로 갈수있는 연결 키가 존재
+	function afterTextRender() {
+
+		if(!isClear){
+			if (node.type == 'lines') {
+				showNextButton();
+			}
+
+			if (node.type == 'choice' && isLastPrompt) {
+				showChoiceButton(node.choices);
+			}
+
+			// 타입이 그냥 줄글이든, 선택이든 마지막 프롬프트에 도달한 시점에 
+			// 통계 데이터 업데이트
+			if (isLastPrompt) {
+				applyEpisodeStats(node);
+			}
+		}else{
+			showClearButtons()
+		}
+		
+	}
+
+}
+
+// 에피소드 랜더링 함수
+function renderEpisode(epId) {
+	let node = STORY.nodes[epId];
+
+	if (!node) {
+		// 노드 객체가 없으면 타이머 종료후 클리어로 이동
+		pauseLifeTimer()
+		node = STORY.nodes[clearEpId];
+	}
+
+	storyArea.innerHTML = '';
+	buttonWrap.innerHTML = '';
+
+	// BGM 변경
+	if (node.bgm) {
+		playBGM(node.bgm);
+	}
+
+	// 첫 프롬프트 출력
+	renderPrompt(node, 0);
+}
+
+// 에피소드 이동 함수
+function goToEpisode(epIds) {
+	let epId;
+
+	// 기존 스트링 형태에서 문자열 배열 형태로 변경
+	if (!epIds || (Array.isArray(epIds) && epIds.length == 0)) {
+		// 1) epIds가 없거나 빈 배열이면 → 클리어 루트
+		epId = null;
+	} else if (Array.isArray(epIds)) {
+		// 2) 배열이면 랜덤 선택
+		const randIndex = Math.floor(Math.random() * epIds.length);
+		epId = epIds[randIndex];
+	} else if (typeof epIds == "string") {
+		// 3) 문자열이면 그대로
+		epId = epIds;
+	} else {
+		// 4) 그 외 타입은 방어
+		epId = null;
+	}
+
+	gameState.currentEpisode = epId;
+	gameState.currentPromptIndex = 0;
+	renderEpisode(epId);
+}
+
+
+// 게임 시작 버튼 클릭
+startBtn.addEventListener('click', async () => {
+	// TODO 로드 스토리 안될때 트라이 캐치로 예외걸기
+	playBGM("/assets/audio/bgm/theme_008.mp3");
+
+	// 노필터 삭제하기
+	imageWrap.classList.remove('no_filter');
+	gameTitle.style.display = 'none';
+	await loadStory();
+	startLifeTimer();     // 생존 타이머 시작
+	goToEpisode('ep1');   // 무조건 ep1 시작
+});
+
+// goToEpisode ->  renderEpisode -> renderPrompt (showNextButton, renderChoices / 통계 누적) 
+
+
+
+
+
+
+
+
+
+
